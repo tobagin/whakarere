c914c77f71251f937fa239be98e155fd59f0d31c
Thiago Fernandes <thiago@avila-fernandes.com>
2023-10-29 19:46:45 +0000

    changes

--- whakarere.py
+++ whakarere.py
@@ -2,8 +2,7 @@ import gi
 gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
 gi.require_version("WebKit", "6.0")
-gi.require_version("GdkPixbuf", "2.0")
-from gi.repository import Gtk, Adw, WebKit, Gio, GdkPixbuf
+from gi.repository import Gtk, Adw, WebKit
 
 BASE64_ICON = ""
 
