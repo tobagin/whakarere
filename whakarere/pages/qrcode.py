c5b8e8a1f9b9896ef1bb611be2a41a8049e4dcf8
Thiago Fernandes <thiago@avila-fernandes.com>
2023-11-18 20:55:03 +0000

    Update qrcode.py

--- whakarere/pages/qrcode.py
+++ whakarere/pages/qrcode.py
@@ -5,7 +5,7 @@ gi.require_version("Adw", "1")
 gi.require_version("GdkPixbuf", "2.0")
 gi.require_version("Gdk", "4.0")
 
-from gi.repository import Gtk, Adw, GdkPixbuf, Gdk, GLib, Gio
+from gi.repository import Gtk, Adw, GdkPixbuf, Gdk, GLib
 from whakarere.widgets.main_menu import MainMenuButtonWidget
 from whakarere.widgets.titlebar import WindowTitlebarWidget
 import threading, time, requests, qrcode
@@ -32,8 +32,6 @@ class QrManagerPage(Adw.NavigationPage):
         self.button_settings_menu = MainMenuButtonWidget()
 
         # Create QR Code Image
-        while not self.check_session_status(session_id):
-            time.sleep(1)
         self.qr_code_texture = self.get_qr_code_texture(self.get_qr_code_data(session_id))
         self.qr_code = Gtk.Picture()
         self.qr_code.set_paintable(self.qr_code_texture)
@@ -70,15 +68,6 @@ class QrManagerPage(Adw.NavigationPage):
         self.check_session_status_thread = threading.Thread(target=self.check_session_status_thread, args=(self.session_id,))
         self.check_session_status_thread.start()
 
-    def get_qr_code_data(self, session_id):
-        url = self.api_url + f'/session/qr/{session_id}'
-        result = ((requests.get(url, headers=self.headers)).json())["qr"]
-
-        if(self.app_manager.is_debug()):
-            print("get_qr_code_data: " + str(result))
-            
-        return result
-    
     def generate_qr_code(self, qr_code_data):
         qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
         qr.add_data(qr_code_data)
@@ -89,8 +78,9 @@ class QrManagerPage(Adw.NavigationPage):
         qr_image = self.generate_qr_code(qr_code_data)
         pixbuf = self.pil_image_to_pixbuf(qr_image)
         return Gdk.Texture.new_for_pixbuf(pixbuf)
-    
+
     def pil_image_to_pixbuf(self, pil_image):
+        """Convert a PIL image to a GdkPixbuf."""
         buffer = BytesIO()
         pil_image.save(buffer, format="PNG")
         glib_bytes = GLib.Bytes.new(buffer.getvalue())
@@ -99,6 +89,15 @@ class QrManagerPage(Adw.NavigationPage):
         pixbuf = loader.get_pixbuf()
         loader.close()
         return pixbuf
+
+    def get_qr_code_data(self, session_id):
+        url = self.api_url + f'/session/qr/{session_id}'
+        result = ((requests.get(url, headers=self.headers)).json())["qr"]
+
+        if(self.app_manager.is_debug()):
+            print("get_qr_code_data: " + str(result))
+            
+        return result
     
     def check_session_status(self, session_id):
         url = self.api_url + f'/session/status/{session_id}'
@@ -107,4 +106,10 @@ class QrManagerPage(Adw.NavigationPage):
         if(self.app_manager.is_debug()):
             print("check_session_status: " + str(result))
             
-        return result 
+        return result
+    
+    def check_session_status_thread(self, session_id):
+        while not self.check_session_status(session_id):
+            time.sleep(2)
+        self.app_manager.main_window.navigation_view.pop()
+        self.app_manager.main_window.session_manager_page.refresh_listview()
